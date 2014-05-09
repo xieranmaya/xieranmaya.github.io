f365269845fc9ab410f410848aabb1d802b977da
xieranmaya <xieranmaya@gmail.com>
2014-05-09 11:16:23 +0800

    beauty the code

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//Define an angular module for our app
 var sampleApp = angular.module('sampleApp', []);
-//Define Routing for app
-//Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
-//Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
+
 sampleApp.config(['$routeProvider', '$locationProvider',
-	function($routeProvider, $locationProvider) {
+	function ($routeProvider, $locationProvider) {
 		$routeProvider.
-		when('/',{
+		when('/', {
 			templateUrl: 'templates/list.html',
-			controller:'List'
+			controller: 'List'
 		}).
 		when('/AddNewOrder', {
 			templateUrl: 'templates/add_order.html',
@@ -27,20 +24,20 @@ sampleApp.config(['$routeProvider', '$locationProvider',
 		$locationProvider.html5Mode(true);
 	}
 ]);
-sampleApp.controller('AddOrderController', function($scope) {
+sampleApp.controller('AddOrderController', function ($scope) {
 	$scope.message = 'This is Add new order screen';
 });
-sampleApp.controller('ShowOrdersController', function($scope) {
+sampleApp.controller('ShowOrdersController', function ($scope) {
 	$scope.message = 'This is Show orders screen';
 });
-sampleApp.controller('otherUrl', function($scope,$http) {
-	$scope.message = 'You are in here: '+location.pathname;
-	$http.get(location.pathname+".txt").success(function(data){
+sampleApp.controller('otherUrl', function ($scope, $http) {
+	$scope.message = 'You are in here: ' + location.pathname;
+	$http.get(location.pathname + ".txt").success(function (data) {
 		$scope.blogContent = data;
 	})
 });
-sampleApp.controller('List',function($scope,$http){
-	$http.get('/posts.json').success(function(data){
-		$scope.posts=data;
+sampleApp.controller('List', function ($scope, $http) {
+	$http.get('/posts.json').success(function (data) {
+		$scope.posts = data;
 	});
 });
