d14721b9f81063ca3ac0fdb0924abc279376b13b
xieranmaya <xieranmaya@gmail.com>
2014-12-04 11:42:34 +0800

    删除无用代码,简单重构

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,5 +1,93 @@
 var Blog = angular.module('Blog', ['ngRoute'])
 
+.config(function ($routeProvider, $locationProvider){
+	$locationProvider.html5Mode(true)
+	$routeProvider.
+		when('/', {// 主页，展示文章列表第一页
+			templateUrl: 'templates/home.html',
+			controller: 'PostList'
+		}).
+		when('/post/:file',{// 文章页面
+			templateUrl: 'templates/post.html',
+			controller: 'Post',
+		}).
+		otherwise({
+			templateUrl: 'templates/404.html'
+		})
+})
+
+.controller('PostList', function ($scope, BlogData) {
+	BlogData.postList().then(function (posts){
+		$scope.posts = posts
+	})
+})
+.controller('Post', function ($scope, BlogData, $routeParams){
+	BlogData.getPost($routeParams.file).then(function(post){
+		$scope.post = post
+	})
+})
+.controller("Blog",function ($scope, BlogData){
+	BlogData.meta().then(function (meta){
+		$scope.meta = meta
+	})
+})
+
+.factory('BlogData', function ($http, $q){
+	// 带缓存功能的数据提供模块
+	blogData = {}
+	blogData.posts = {}
+	return {
+		meta:function(){
+			return $q(function(resolve){
+				if(blogData.meta){
+					resolve(blogData.meta)
+				}else{
+					$http.get('generated/meta.json').success(function(meta){
+						blogData.meta = meta
+						resolve(meta)
+					})
+				}
+			})
+		},
+		postList:function(){
+			return $q(function(resolve){
+				if(blogData.postList){
+					resolve(blogData.postList)
+				}else{
+					$http.get('posts/test/g/all.json').success(function(postList){
+						blogData.postList = postList
+						resolve(postList)
+						postList.forEach(function(post){
+							blogData.posts[post.file] = post
+						})
+					})
+				}
+			})
+		},
+		getPost:function(file){
+			return $q(function(resolve){
+				if(!blogData.posts[file]){
+					$http.get('/posts/test/g/' + file + '.md.json').success(function(post){
+						blogData.posts[file] = post
+					}).then(function(){
+						$http.get('/posts/test/' + file + '.md').success(function (content){
+							blogData.posts[file].content = content.replace(/[\s\S]*\-\-\-/igm, '')
+							resolve(blogData.posts[file])
+						})
+					})
+				}else if(!blogData.posts[file].content){
+					$http.get('/posts/test/' + file + '.md').success(function (content){
+						blogData.posts[file].content = content.replace(/[\s\S]*\-\-\-/igm, '')
+						resolve(blogData.posts[file])
+					})
+				}else{
+					resolve(blogData.posts[file])
+				}
+			})
+		}
+	}
+})
+
 .directive('markdown', function () {
     var converter = new Showdown.converter()
     return {
@@ -22,120 +110,15 @@ var Blog = angular.module('Blog', ['ngRoute'])
 	}
 })
 
-.config(function ($routeProvider, $locationProvider){
-	$routeProvider.
-		when('/', {// 主页，展示文章列表第一页
-			templateUrl: 'templates/home.html',
-			controller: 'PostList'
-		}).
-		when('/post/:file',{// 文章页面
-			templateUrl: 'templates/post.html',
-			controller: 'Post',
-			resolve:{
-			}
-		}).
-		when('/pages',{// 页面列表
-			templateUrl: 'templates/pages.html'
-		}).
-		when('/page/:page',{// 页面
-			templateUrl: 'templates/page.html',
-			controller: 'Page'
-		}).
-		// when('/:page',{// 文章列表翻页
-		// 	templateUrl: 'templates/post-list.html',
-		// 	controller: 'PostList'
-		// }).
-		otherwise({
-			templateUrl: 'templates/404.html'
-		})
-
-	$locationProvider.html5Mode(true)
-})
-// .controller('Home', function ($scope, $http){
-// 	$http.get('generated/posts.json').success(function(posts){
-// 		$scope.posts = posts
-// 	})
-// })
-.controller('PostList', function ($scope, BlogData, $routeParams) {
-	BlogData.postList().then(function (posts){
-		//(posts = data.split('\n')).pop()
-		$scope.posts = posts
-	})
-})
-.controller('Post', function ($scope, BlogData, $routeParams){
-	BlogData.getPost($routeParams.file).then(function(post){
-		$scope.post = post
-	})
-	// $http.get('/posts/test/g/' + $routeParams.file + '.md.json').success(function(post){
-	// 	$scope.post = post
-	// }).then(function(){
-	// 	$http.get('/posts/test/' + $routeParams.file + '.md').success(function (content){
-	// 		$scope.post.content = content.replace(/[\s\S]*\-\-\-/igm, '')
-	// 	})
-	// })
-})
-.controller("Blog",function ($scope, BlogData, $rootScope){
-	BlogData.meta().success(function (meta){
-		$scope.meta = meta
-	})
-})
-.factory('BlogData', function ($http, $q){
-	blogData = {}
-	blogData.posts = {}
-	return {
-		meta:function(){
-			if(blogData.meta){
-				return blogData.meta
-			}
-			return $http.get('generated/meta.json')
-		},
-		postList:function(){
-			return $q(function(resolve){
-				if(blogData.postList){
-					resolve(blogData.postList)
-				}else{
-					$http.get('posts/test/g/all.json').success(function(postList){
-						blogData.postList = postList
-						resolve(postList)
-						postList.forEach(function(post){
-							blogData.posts[post.file] = post
-						})
-					})
-				}
-			})
-		},
-		getPost:function(file){
-			return $q(function(resolve){
-				if(!blogData.posts[file]){
-					$http.get('/posts/test/g/'+file+'.md.json').success(function(post){
-						blogData.posts[file] = post
-					}).then(function(){
-						$http.get('/posts/test/' + file + '.md').success(function (content){
-							blogData.posts[file].content = content.replace(/[\s\S]*\-\-\-/igm, '')
-							resolve(blogData.posts[file])
-						})
-					})
-				}else if(!blogData.posts[file].content){
-					$http.get('/posts/test/' + file + '.md').success(function (content){
-						blogData.posts[file].content = content.replace(/[\s\S]*\-\-\-/igm, '')
-						resolve(blogData.posts[file])
-					})
-				}else{
-					resolve(blogData.posts[file])
-				}
-			})
-		}
-	}
-})
-
 .directive('disqus',function(){
 	return {
 		restrict:'A',
 		link:function(scope, el, attrs){
-			DISQUS.reset({reload:true});
+			DISQUS.reset({reload:true})
 		}
 	}
 })
+
 .directive('duoshuo',function(){
 	return {
 		restrict:'A',
@@ -144,7 +127,7 @@ var Blog = angular.module('Blog', ['ngRoute'])
 				return el.attr('data-thread-key')
 			},function(newVal){
 				if(newVal){
-					DUOSHUO.EmbedThread(el[0]);
+					DUOSHUO.EmbedThread(el[0])
 				}
 			});
 		}
